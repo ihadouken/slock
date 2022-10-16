5c5278ae124da262d33dc2d6a513944813677f46
Rahul Sharma <rsharma842756@gmail.com>
2022-10-16 08:16:41 +0530

    increase blur radius

--- config.h
+++ config.h
@@ -50,7 +50,7 @@ static XRectangle rectangles[9] = {
 /*Enable blur*/
 #define BLUR
 /*Set blur radius*/
-static const int blurRadius=5;
+static const int blurRadius=10;
 /*Enable Pixelation*/
 //#define PIXELATION
 /*Set pixelation radius*/
